c1ecfcbbf0e5b93f105f813ef43c88b461d7e027
Tanja7 <109756465+Tanja7@users.noreply.github.com>
2023-01-17 16:48:29 +0300

    Main + ReadMe

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {50, 28, 167};
+        int[] sales1 = {150, 328, 67};
+        int[] sales2 = {500, 208, 367};
+
+        SalesManager manager = new SalesManager(sales);
+        SalesManager manager1 = new SalesManager(sales1);
+        SalesManager manager2 = new SalesManager(sales2);
+
+        System.out.println("manager max - " + manager.max());
+        System.out.println("manager1 max - " + manager1.max());
+        System.out.println("manager2 max - " + manager2.max());
+
+    }
+}
