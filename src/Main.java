addcf8437995b0672505ffc03192783cf8b9762e
Tanja7 <109756465+Tanja7@users.noreply.github.com>
2023-01-18 10:35:41 +0300

    long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {50, 28, 167};
-        int[] sales1 = {150, 328, 67};
-        int[] sales2 = {500, 208, 367};
+        long[] sales = {50, 28, 167};
+        long[] sales1 = {150, 328, 67};
+        long[] sales2 = {500, 208, 367};
 
         SalesManager manager = new SalesManager(sales);
         SalesManager manager1 = new SalesManager(sales1);
