b0ef2913788a6040db9c42a03a9e9e9aebd54e60
Tanja7 <109756465+Tanja7@users.noreply.github.com>
2023-01-18 10:54:00 +0300

    trimmed Average

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {50, 28, 167};
+        int[] sales = {50, 28, 167, 100, 120};
         int[] sales1 = {150, 328, 67};
         int[] sales2 = {500, 208, 367};
 
@@ -11,6 +11,10 @@ public class Main {
         System.out.println("manager max - " + manager.max());
         System.out.println("manager1 max - " + manager1.max());
         System.out.println("manager2 max - " + manager2.max());
+
+        System.out.println("manager trAv - " + manager.trimmedAverage());
+        System.out.println("manager1 trAv - " + manager1.trimmedAverage());
+        System.out.println("manager2 trAv - " + manager2.trimmedAverage());
 
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,5 +14,30 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = max();
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+    // расчёт обрезанного среднего
+    public int trimmedAverage (){
+        int sumSales = 0;
+        for (int sale : sales) {
+            sumSales += sale;
+        }
+        sumSales = sumSales - max() - min();
+        int trimmedAverage = sumSales / (sales.length - 2);
+        return trimmedAverage;
+
+    }
+
 }
+// Добавьте расчёт обрезанного среднего - это среднее количество продаж среди всех продаж,
+// за исключением самой максимальной и самой минимальной. Это иногда используется в статистике
+// для того, чтобы снизить влияение единичных скачков. При этом считать вы можете всё в целых числах.
 
